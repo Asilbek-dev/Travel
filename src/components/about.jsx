2236e35e264aa5c4e0b80e0926cf711d8b369031
Asilbek <ergashevasilbek41@gmail.com>
2021-08-03 11:15:33 +0500

    finish

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,28 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/about.css";
 
 import Slider from "infinite-react-carousel";
 
 const images = [
-    {
-        image: "./images/picture1.png"
-    },
-    {
-        image: "./images/picture2.png"
-    },
-    {
-        image: "./images/picture3.png"
-    },
-    {
-        image: "./images/image 30.png"
-    },
-    {
-        image: "./images/image 31.png"
-    },
-]
-
+  {
+    image: "./images/picture1.png",
+  },
+  {
+    image: "./images/picture2.png",
+  },
+  {
+    image: "./images/picture3.png",
+  },
+  {
+    image: "./images/image 30.png",
+  },
+  {
+    image: "./images/image 31.png",
+  },
+];
 
 export default function About() {
+  
+  const [count, setcount] = useState(true)
+
+  const handleClick = () => {
+    setcount(!count);
+  }
+
   return (
     <>
       <div className="container">
@@ -42,33 +48,43 @@ export default function About() {
             </Slider>
           </div>
           <div className="col about_carousel mb-5">
-            <div className="about_card">
-                <p>
-                “On the Windows talking painted pasture yet its express parties use. Sure last upon he same as knew next. Of believed or diverted no.”
-                </p>
-                <br />
-                <p>Mike taylor</p>
-                <p>Lahore, Pakistan</p>
+            <div className= {count ? "about_card" : "carousel_card1 about_card"}>
+              <p>
+                “On the Windows talking painted pasture yet its express parties
+                use. Sure last upon he same as knew next. Of believed or
+                diverted no.”
+              </p>
+              <br />
+              <p>Mike taylor</p>
+              <p>Lahore, Pakistan</p>
             </div>
-            <div className="about_card1">
-                <p>
-                “On the Windows talking painted pasture yet its express parties use. Sure last upon he same as knew next. Of believed or diverted no.”
-                </p>
-                <br />
-                <p>Chris, Thomas</p>
-                <p>CEO of Red Button</p>
+            <div className= {count ? "about_card1" : "carousel_card2 about_card1"}>
+              <p>
+                “On the Windows talking painted pasture yet its express parties
+                use. Sure last upon he same as knew next. Of believed or
+                diverted no.”
+              </p>
+              <br />
+              <p>Chris, Thomas</p>
+              <p>CEO of Red Button</p>
+            </div>
+            <div className="carousel_next_prev d-block">
+              <div>
+                <i className = {count ? "icon_color fas fa-angle-up" : "fas fa-angle-up"} onClick = {handleClick}></i>
+              </div>
+              <i className = {count ? "fas fa-angle-down mt-5" : "icon_color fas fa-angle-down mt-5"} onClick = {handleClick}></i>
             </div>
             <img src="./images/Image 5.png" alt="" />
           </div>
         </div>
         <div className="row my-5">
-            <div className="col-12 d-flex justify-content-around">
-                {images.map(({image}) => (
-                    <div className="about_logo mt-5">
-                        <img src= {image} alt="" />
-                    </div>
-                ))}
-            </div>
+          <div className="col-12 d-flex justify-content-around">
+            {images.map(({ image }) => (
+              <div className="about_logo mt-5">
+                <img src={image} alt="" />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </>
